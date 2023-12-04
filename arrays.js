9f429ffd21e38641b022a491967e8449bc54c5b2
Bridoppp <tini.pham99@gmail.com>
2023-12-04 16:18:36 +0100

    Arrays Finished

--- arrays.js
+++ arrays.js
@@ -2,16 +2,27 @@
 // 1. Declareer een variabele en sla daar een array met vier jaartallen (zoals 2021) in op. Log dit in de terminal.
 // ==========================================
 
+const years = [2000, 2001, 2002, 2003];
+
+console.log(years)
+
 
 // ==========================================
 // 2. Declareer een variabele en sla daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
 // ==========================================
+
+const hobbies= ["skating", "boardgames", "dates"];
+console.log(hobbies)
 
 
 // ==========================================
 // 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
 // 3b. Zorg er nu voor dat je de eerste waarde uit die array in de terminal logt             // geeft 3
 // ==========================================
+
+const firstArray= [3,4,5,7,2]
+
+console.log(firstArray[0]);
 
 
 // ==========================================
@@ -20,6 +31,10 @@
 // 4b. Zorg ervoor dat je de vijfde waarde uit bovenstaande array in de terminal logt       // geeft blauw
 // 4c. Zorg ervoor dat je de tweede waarde uit bovenstaande array in de terminal logt       // geeft geel
 // ==========================================
+const colors=["green", "yellow","red","purple","blue","orange"]
+
+console.log(colors[4]);
+console.log(colors[1]);
 
 
 // ==========================================
@@ -28,10 +43,24 @@
 // 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
 
+const numberList=[21, 22, 23, 25, 25];
+
+console.log(numberList);
+
+numberList[3]=24;
+console.log(numberList);
+
 
 // ==========================================
 // 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
 // 6b. Overschrijf daarna de waarde bladerdeeg met lasagne bladen (je mag de declaratie op de vorige regel niet aanpassen).
-// 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
+// 6c. Log de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+
+const ingredients = ["bladerdeeg","knoflook","spinazie"];
+console.log(ingredients);
+ingredients[0]= "lasagne bladen";
+console.log(ingredients);
+
+console.log(ingredients.length);
